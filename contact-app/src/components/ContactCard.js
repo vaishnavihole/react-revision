f9bf82bca2175539f76843e01b92c15fce2db34d
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-27 12:45:30 +0530

    add jsx

--- contact-app/src/components/ContactCard.js
+++ contact-app/src/components/ContactCard.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const CardContact = () => {
+   
+    return (
+        <div className="item">
+        <div className="content">
+            <div className="header">{contact.name}</div>
+            <div>{contact.email}</div>
+        </div>
+        <i className="trash alternate outline icons"></i>
+    </div>
+    );
 
-}
+
+};
 
 export default ContactCard;
