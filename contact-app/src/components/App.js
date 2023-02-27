b2eb8187ee46dcabbe1211074d4dfd9e5ab4462f
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-27 13:47:07 +0530

    use useEffect

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -5,12 +5,17 @@ import AddContact from './AddContact';
 import ContactList from'./ContactList';
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts"
   const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, contact])
-  }
+  };
+
+  useEffect(() =>{
+    localStorage.setItem()
+  },[contacts]);
   return(
     <div className='ui container'>
       <Header/>
