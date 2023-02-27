c3c6aae733f539fd4b98e6a3887c3132fec0b693
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-27 12:04:50 +0530

    export addcontact components

---
+++ contact-app/src/components/AddContact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class  AddContact extends React.Component {
+    render() {
+        return (
+            <div className="ui main">
+                <h2>Add Contact</h2>
+                
+            </div>
+        )
+    }
+} 
+         
+
+
+export default AddContact;
--- contact-app/src/components/Header.js
+++ contact-app/src/components/Header.js
@@ -1,9 +1,13 @@
-import react from react
+import react from "react";
 
-const Header = () =>{
+const Header = () => {
     return(
         <div className="ui fixed menu">
-            
+            <div className="ui container center">
+                <h2>Contact Manager</h2>
+            </div>
         </div>
     );
 }
+
+export default Header;
