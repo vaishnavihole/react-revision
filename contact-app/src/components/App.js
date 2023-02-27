e806656b9c223f61b31b613331039ee7439985a2
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-27 17:58:32 +0530

    install react-router-dom

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react'
-import { uuid } from 'uuidv4';
+// import { uuid } from 'uuidv4';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
@@ -11,7 +11,7 @@ function App() {
 
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts, {id: uuid(), ...contact}]);
+    // setContacts([...contacts, {id: uuid(), ...contact}]);
   };
 
   const removeContactHandler = (id) => {
