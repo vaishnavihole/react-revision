76212553f64cf35d6d8c3d14ee6b4708a44fd54d
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-27 12:54:27 +0530

    use destructing

--- contact-app/src/components/ContactCard.js
+++ contact-app/src/components/ContactCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const CardContact = (props) => {
-   
+   const {id, name, email} = props.contact;
     return (
         <div className="item">
         <div className="content">
-            <div className="header">{contact.name}</div>
-            <div>{contact.email}</div>
+            <div className="header">{name}</div>
+            <div>{email}</div>
         </div>
         <i className="trash alternate outline icons"
         style={{color:"red", marginTop: "7px"}}></i>
