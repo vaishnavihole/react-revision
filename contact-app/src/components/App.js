5b27dfa4f087902ce5f303a114fcf1575b70d2e0
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-27 11:33:15 +0530

    import components

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,9 +1,11 @@
 import react from 'react'
 import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from'./ContactList';
 
 function App() {
   return  <div>Hello Word</div>
-
 }
 
 export default App;
