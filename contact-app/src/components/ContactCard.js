a9b708ce6e965b2ca0adf3f04a95d85cb792a34f
vaishnavihole <vaishnavihole1@gmail.com>
2023-02-28 15:57:59 +0530

    use link for route

--- contact-app/src/components/ContactCard.js
+++ contact-app/src/components/ContactCard.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ContactCard = (props) => {
    const {id, name, email} = props.contact;
     return (
         <div className="item">
         <div className="content">
+            <Link to={`/contact/${id}`}>
             <div className="header">{name}</div>
             <div>{email}</div>
+            </Link>
         </div>
         <i className="trash alternate outline icons"
         style={{color:"red", marginTop: "7px"}}></i>
